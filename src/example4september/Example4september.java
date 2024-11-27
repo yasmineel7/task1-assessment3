02fbf614c6d00cde11694279b0a06ae13f006724
yasmi <yasmi@LAPTOP-3ELE7G2B>
2024-11-26 21:55:31 -0500

    hi

--- src/example4september/Example4september.java
+++ src/example4september/Example4september.java
@@ -12,80 +12,83 @@ import java.util.Scanner;
  */
 public class Example4september {
 
-/**
-     * @param c
+ /**
+     * @param word
      * @return 
      */
-    public static int letterToNumber(char c) {
-         c = Character.toLowerCase(c);
-        return c - 'a';
+    public static String convertCase(String word){
+        String word2 = word.toLowerCase();
+        char firstChar = word2.charAt(0);
+        char firstChar1 = Character.toTitleCase(firstChar);
+        String word3 = firstChar1 + word2.substring(1, word2.length());
+        return word3;
     }
     
-    
-    public static boolean isDivisible(int num){
-               return num % 3 == 0;
-    }
-       
-    public static boolean isDivisible(int num, int base) {
-        return num % base == 0;
-    }
-    
-    public static boolean isDivisible(char letter) {
-        return letterToNumber(letter) % 3 == 0; 
-    }
-    
-    public static boolean isDivisible(char letter, int base) {
-       return letterToNumber(letter) % base == 0;
-    }
+    public static String convertCase(String word, char caseType){
+        
+        switch (caseType){
+             case 'l' -> {
+                 String word1 = word.toLowerCase();
+                 return word1;
+            }
+            case 'L' -> {
+                String word1 = word.toLowerCase();
+                 return word1;
+            }
+            case 'u' -> {
+               String word1 = word.toUpperCase();
+                 return word1;
+            }
+            case 'U' -> {
+                String word1 = word.toUpperCase();
+                 return word1;
+            }
+            case 't' -> {
+                String word2 = word.toLowerCase();
+        char firstChar = word2.charAt(0);
+        char firstChar1 = Character.toTitleCase(firstChar);
+        String word3 = firstChar1 + word2.substring(1, word2.length());
+        return word3;
+            }
+            case 'T' -> {
+                String word2 = word.toLowerCase();
+        char firstChar = word2.charAt(0);
+        char firstChar1 = Character.toTitleCase(firstChar);
+        String word3 = firstChar1 + word2.substring(1, word2.length());
+        return word3;
+            }
             
-    public static boolean isDivisible(char letter, char base){
-        return letterToNumber(letter) % letterToNumber(base) == 0;
+            default -> System.out.println("Error");
+           
+    }
+        return null;
+           
     }
     public static void main(String[] args) {
         // TODO code application logic here
-         //Create an object scanner 
+         // TODO code application logic here
+        //Create an object scanner
         Scanner input = new Scanner(System.in);
-        //Prompt the user to enter a number and a base
-        System.out.printf("Please enter a number and a base separated by a space: ");
-        int num = input.nextInt();
-        int base = input.nextInt();
         
+        //Prompt the user to enter a word
+        System.out.printf("Please enter a word: ");
+        String word = input.nextLine();
+        
+        //Prompt the user to choose a case
+        System.out.println("Please choose the case you want to convert: "); 
+        System.out.println("1. 'l' or 'L'for lowercase");
+        System.out.println("2.'u' or 'U' for uppercase");
+        System.out.println("3. 't' or 'T' for titlecase"); 
+        char caseType = input.next().charAt(0); 
+       
         //Display the result
-        if (isDivisible(num)) 
-            System.out.println(num + " is divisible by 3" );
-         else 
-            System.out.println(num + " is not divisible by 3" );
-        
-        if (isDivisible(num, base) && 3!=base){
-        System.out.println(num+ " is  divisible by " + base); 
-        }
-        
-        if (isDivisible(num, base) == false && 3!=base)
-        System.out.println(num + " is  not divisible by " + base);
-        
-        
-         input.nextLine();
-        
+        input.nextLine(); input.nextLine();
+        System.out.printf("%-38s : %s","Original word ", word); 
         input.nextLine();
-        
-        //Prompt the user to enter an english letter, an number base and an English letter base 
-        System.out.printf("Please enter an English letter: ");
-        char c = input.next().charAt(0);
-        System.out.printf("Please enter an number base and an English letter base, separated by a space: ");
-        int num2 = input.nextInt();
-        char letter = input.next().charAt(0); input.nextLine();
+        System.out.printf("%-38s : %s","Calling the first convertCase method", convertCase(word));
         input.nextLine();
-        
-        //Display the result
-        if (isDivisible(c, num2))
-            System.out.println("'" + c + "'" + " is divisible by "+ num2 );
-        else
-            System.out.println("'" + c + "'" + " is not divisible by " + num2);
-        
-        if (isDivisible(c, letter))
-            System.out.println("'" + c + "'" + " is divisible by " + "'" + letter + "'");
-        else
-            System.out.println("'" + c + "'" + " is divisible by " + "'" + letter + "'");
+        System.out.printf("%-38s : %s","Calling the second convertCase method", convertCase(word,caseType)); 
+        input.nextLine();
         
     }
 }
